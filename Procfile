8665cd33ea76cda821b7027218d6f4e743aa2908
Qing Wu <wiserfirst@gmail.com>
2019-11-27 16:27:20 +1100

    update for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: MIX_ENV=prod POOL_SIZE=2 mix ecto.migrate; npm install
 web: MIX_ENV=prod mix phx.server
--- apps/rubiks/config/prod.exs
+++ apps/rubiks/config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :rubiks, Rubiks.Repo,
-  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
