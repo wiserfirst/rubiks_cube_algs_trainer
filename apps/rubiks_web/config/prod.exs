969b42f4e05d8928d56fef62853599f230e51fc5
Qing Wu <wiserfirst@gmail.com>
2019-11-28 14:38:19 +1100

    use custom domain

--- apps/rubiks_web/config/prod.exs
+++ apps/rubiks_web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rubiks_web, RubiksWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  url: [host: "rubiks.wiserfirst.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
