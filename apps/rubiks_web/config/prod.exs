677a7e98f274d294d8a0dd17b0b830ab769cc7c5
Qing Wu <wiserfirst@gmail.com>
2019-11-28 14:32:38 +1100

    force ssl for gigalixir domain

--- apps/rubiks_web/config/prod.exs
+++ apps/rubiks_web/config/prod.exs
@@ -16,5 +16,6 @@ use Mix.Config
 config :rubiks_web, RubiksWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
